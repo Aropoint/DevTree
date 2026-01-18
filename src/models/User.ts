dd81f7c2628813d24a02d1341ba4feb9e2da18a3
AroPoint <samuelpozas7@gmail.com>
2026-01-18 03:16:30 -0300

    APO: Hash y Handlers agregados

--- src/models/User.ts
+++ src/models/User.ts
@@ -1,12 +1,20 @@
 import mongoose, {Schema} from "mongoose";
 
 interface IUser {
+    handle: string
     name: string
     email: string
     password: string
 }
 
 const userSchema = new Schema({
+    handle: {
+        type: String,
+        trim: true,
+        required: true,
+        lowercase: true,
+        unique: true
+    },
     name: {
         type: String,
         trim: true,
@@ -16,7 +24,8 @@ const userSchema = new Schema({
         type: String,
         trim: true,
         required: true,
-        unique: true
+        unique: true,
+        lowercase: true
     },
     password: {
         type: String,
