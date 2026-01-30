efa77c718fd8ef3f537357a624bb6941c3a3999a
AroPoint <samuelpozas7@gmail.com>
2026-01-30 01:09:15 -0300

    Implementación de vistas de autenticación y manejo de formularios con validación

--- FrontEnd/src/Views/LoginView.tsx
+++ FrontEnd/src/Views/LoginView.tsx
@@ -3,9 +3,11 @@ import {Link} from 'react-router-dom';
 export default function LoginView() {
   return (
     <>
-        <div className='text-blue-500'>LoginView</div>
-        <nav>
-            <Link to="/auth/register">cuenta</Link>
+        <h1 className="text-4xl text-white font-bold">Iniciar Sesión</h1>
+        <nav className="mt-10">
+            <Link
+              className="text-center text-white text-lg block"
+              to="/auth/register">¿No tienes una cuenta? Regístrate</Link>
         </nav>
     </>
 
