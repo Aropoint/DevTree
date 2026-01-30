a7adec04228bb8bee60ab5ca1ceef54227e069da
AroPoint <samuelpozas7@gmail.com>
2026-01-29 22:08:53 -0300

    Cambio en la estructura del Proyecto

--- src/index.ts
+++
@@ -1,9 +0,0 @@
-import colors from 'colors';
-import server from './server';
-
-const port = process.env.PORT || 3000;
-
-server.listen(port, () => {
-    console.log(colors.blue.bold('El Servidor está funcionando en el puerto: ' + port))
-})
-
