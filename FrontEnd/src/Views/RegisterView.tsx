b9de0ac0697b059af1c36517d3409644d7f66039
AroPoint <samuelpozas7@gmail.com>
2026-01-31 00:52:01 -0300

    Manejo de errores en validación y mejora en la vista de registro con notificaciones

--- FrontEnd/src/Views/RegisterView.tsx
+++ FrontEnd/src/Views/RegisterView.tsx
@@ -2,7 +2,9 @@ import { Link } from "react-router-dom"
 import {useForm} from 'react-hook-form';
 import ErrorMessage from "../components/ErrorMessage";
 import type { RegisterForm } from "../types";
-import axios, {isAxiosError} from "axios";
+import {isAxiosError} from "axios";
+import {toast} from "sonner";
+import api from "../config/Axios";
 
 export default function RegisterView() {
     const initialValues = {
@@ -12,19 +14,19 @@ export default function RegisterView() {
         password: '',
         password_confirmation: ''
     }
-    
+
     const {register, watch, reset, handleSubmit, formState: {errors}} = useForm({defaultValues : initialValues});
 
     const password = watch('password');
 
     const handleRegister = async (formData : RegisterForm) => {
         try {
-            const {data} = await axios.post(`${import.meta.env.VITE_API_URL}/auth/register`, formData);
-            console.log(data);
+            const {data} = await api.post(`/auth/register`, formData);
+            toast.success(data.msg)
             reset()
         } catch (error) {
             if(isAxiosError(error) && error.response) {
-                console.log(error.response.data.error)
+                toast.error(error.response.data.error)
             }
         }
     }
@@ -32,7 +34,7 @@ export default function RegisterView() {
     return (
 
     <>
-        <h1 className="text-4xl text-white font-bold">Crear Cuenta</h1>
+        <h1 className="text-4xl text-white font-bold text-center">Crear Cuenta</h1>
         
         <form 
             onSubmit={handleSubmit(handleRegister)}
--- FrontEnd/src/layouts/AuthLayout.tsx
+++ FrontEnd/src/layouts/AuthLayout.tsx
@@ -1,8 +1,10 @@
 import {Outlet} from 'react-router-dom';
+import {Toaster} from 'sonner'
 
 export default function AuthLayout() {
   return (
     <>
+        <Toaster position='top-right' richColors/>
         <div className='bg-slate-800 min-h-screen'>
             <div className='max-w-lg mx-auto pt-10 px-5'>
                 <img src='/logo.svg' alt='Logo DevTree'/>
