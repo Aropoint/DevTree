c3664c40e5b2789b7f348f1afab0debf2e237807
AroPoint <samuelpozas7@gmail.com>
2026-01-31 01:16:04 -0300

    Término de vista y funcionamiento de Login y Register

--- FrontEnd/src/Views/RegisterView.tsx
+++ FrontEnd/src/Views/RegisterView.tsx
@@ -7,7 +7,7 @@ import {toast} from "sonner";
 import api from "../config/Axios";
 
 export default function RegisterView() {
-    const initialValues = {
+    const initialValues : RegisterForm= {
         name: '',
         email: '',
         handle: '',
